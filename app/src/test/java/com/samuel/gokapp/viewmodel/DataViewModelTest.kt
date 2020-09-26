ece2e53f9ebc5cc78c9eb069ccecea5a71e015f4
Samuel Ribeiro <samuel8mille@gmail.com>
2020-09-26 18:13:53 -0300

    implementa teste

---
+++ app/src/test/java/com/samuel/gokapp/viewmodel/DataViewModelTest.kt
@@ -0,0 +1,59 @@
+package com.samuel.gokapp.viewmodel
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import com.samuel.gokapp.model.gokrepository.CashModel
+import com.samuel.gokapp.model.gokrepository.DataModel
+import com.samuel.gokapp.model.gokrepository.ProductsModel
+import com.samuel.gokapp.model.gokrepository.SpotlightModel
+import com.samuel.gokapp.model.gokrepository.service.GokApiService
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import io.reactivex.Single
+import io.reactivex.android.plugins.RxAndroidPlugins
+import io.reactivex.functions.Function
+import io.reactivex.schedulers.Schedulers
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class DataViewModelTest {
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    private val repository = mockk<GokApiService>()
+    private val onDataLoadedObserver = mockk<Observer<DataModel>>(relaxed = true)
+    private val dataModel = DataModel(
+        listOf(SpotlightModel("", "", "")),
+        listOf(ProductsModel("", "", "")),
+        CashModel("", "", "")
+    )
+
+    @Before
+    fun beforeTest() {
+        RxAndroidPlugins.setInitMainThreadSchedulerHandler(Function { Schedulers.trampoline() })
+    }
+
+    @Test
+    fun `when view model fetches data than it should call the repository`() {
+        val viewModel = instantiateViewModel()
+
+        val singleDataModel = Single.just(dataModel)
+
+        every { repository.getData() } returns singleDataModel
+
+        viewModel.fetchFromRemote()
+
+        verify { repository.getData() }
+        verify { onDataLoadedObserver.onChanged(dataModel) }
+
+    }
+
+    private fun instantiateViewModel(): DataViewModel {
+        val viewModel = DataViewModel(repository)
+        viewModel.data.observeForever(onDataLoadedObserver)
+        return viewModel
+    }
+}
